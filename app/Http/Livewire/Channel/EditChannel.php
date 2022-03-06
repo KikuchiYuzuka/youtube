e09ef046fbf89d3cbf2d450b965c1df47972e827
KikuchiYuzuka <82356012+KikuchiYuzuka@users.noreply.github.com>
2022-03-06 13:28:07 +0900

    updateの機能追加

--- app/Http/Livewire/Channel/EditChannel.php
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -3,12 +3,25 @@
 namespace App\Http\Livewire\Channel;
 
 use App\Models\Channel;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
 
 class EditChannel extends Component
 {
+    use AuthorizesRequests;
 
     public $channel;
+    public $image;
+
+    protected function rules()
+    {
+        return [
+            'channel.name' => 'required|max:255|unique:channels,name,' . $this->channel->id,
+            'channel.slug' => 'required|max:255|unique:channels,slug,' . $this->channel->id,
+            'channel.description' => 'nullable|max:1000',
+            'image' => 'nullable|image|max:1024',
+        ];
+    }
 
     public function mount(Channel $channel)
     {
@@ -18,5 +31,21 @@ class EditChannel extends Component
     public function render()
     {
         return view('livewire.channel.edit-channel');
+    }
+
+    public function update()
+    {
+        $this->authorize('update', $this->channel);
+        $this->validate();
+
+        $this->channel->update([
+            'name' => $this->channel->name,
+            'slug' => $this->channel->slug,
+            'description' => $this->channel->description,
+        ]);
+
+        session()->flash('message', 'Channel updated');
+
+        return redirect()->route('channel.edit', ['channel' => $this->channel->slug]);
     }
 }
