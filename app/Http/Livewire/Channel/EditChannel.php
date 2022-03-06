25f12b27f3dbe2aa640ea8d3db4a7e471dc059eb
KikuchiYuzuka <82356012+KikuchiYuzuka@users.noreply.github.com>
2022-03-06 22:18:27 +0900

    intervention.ioのダウンロードと画像の表示機能追加

--- app/Http/Livewire/Channel/EditChannel.php
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Channel;
 
 use App\Models\Channel;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Intervention\Image\Facades\Image;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -48,8 +49,14 @@ class EditChannel extends Component
 
         if ($this->image) {
             $image = $this->image->storeAd('images', $this->channel->uid . '.png');
+            $imageImage = explode('/', $image)[1];
+            Image::make(storage_path() . '/app/' . $image)
+                ->encode('png')
+                ->fit(80, 80, function($constraint) {
+                $constraint->upsize()->save();
+            });
             $this->channel->update([
-                'image' => $image
+                'image' => $imageImage
             ]);
         }
         session()->flash('message', 'Channel updated');
